00528e09429966f6a216fe4553876fd6e17ffaca
Athul P <athul929@gmail.com>
2018-01-09 18:41:05 +0530

    Update heap.c

--- Heap/heap.c
+++ Heap/heap.c
@@ -1,8 +1,6 @@
-
 #include <stdio.h>
 #include<stdbool.h>
-
-#define HEAP_SIZE 10000
+#include<stdlib.h>
 
 void max_heapify(int *heap, int node, int heap_size)
 {
@@ -42,21 +40,21 @@ void build_max_heap(int heap[], int heap_size)
 
 int find_maximum(int heap[], int *heap_size)
 {
-    int temp = heap[heap_size - 1];
-    heap[heap_size-1] = heap[0];
+    int temp = heap[*heap_size - 1];
+    heap[*heap_size-1] = heap[0];
     heap[0] = temp;
-    heap_size -- ;
-    max_heapify(heap, 1, heap_size);
-    return heap[heap_size - 1]
+    *heap_size -- ;
+    max_heapify(heap, 1, *heap_size);
+    return heap[*heap_size - 1];
+}
 int main()
 {
    int heap_size = 100000;
-   int heap[HEAP_SIZE];
-    for (int i = 0; i < HEAP_SIZE; i++) 
+   int heap[heap_size];
+    for (int i = 0; i < heap_size; i++) 
         heap[i] = rand()%100;
-   build_max_heap(heap, HEAP_SIZE);
-   int max= find_maximum(heap, &heap_size)
+   build_max_heap(heap, heap_size);
+   int max= find_maximum(heap, &heap_size);
+}  
     
-    
-   
-}
+  
