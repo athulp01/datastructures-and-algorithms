f88bc70e552e5a64b7aa7e99e71ca53ba755069a
Athul P <athul929@gmail.com>
2018-01-09 18:18:02 +0530

    include build_max_heap()

--- Heap/heap.c
+++ Heap/heap.c
@@ -1,31 +1,49 @@
 
 #include <stdio.h>
+#include<stdbool.h>
 
 void max_heapify(int *heap, int node, int heap_size)
 {
+    int right = 2*node+1;
+    int left = 2*node;
     if(node <= heap_size)
     {
-        int right = 2*node+1;
-        int left = 2*node;
-        if(heap[node]<(heap[right]<heap[left]?heap[left]:heap[right]))
+        bool rightl=0;
+        rightl = heap[right-1]>heap[left-1]?1:0;
+        if(rightl&&heap[node-1]< heap[right-1] && right <= heap_size)
         {
-            int temp = heap[node];
-            heap[node] = heap[right];
-            heap[right] = temp;
-            max_heapify(heap, right, heap_size);
+            int temp = heap[node-1];
+            heap[node-1] = heap[right-1];
+            heap[right-1] = temp;
+            printf("swaped %d and %d\n", heap[node-1], heap[right-1]);
+            max_heapify(heap, right , heap_size);
+            
         }
+        if(!rightl && heap[node-1]< heap[left-1] && left<=heap_size)
+        {
+            int temp = heap[node-1];
+            heap[node-1] = heap[left-1];
+            heap[left-1] = temp;
+             printf("swaped %d and %d\n", heap[node-1], heap[left-1]);
+            max_heapify(heap, left , heap_size);
+           
+        }
+        
     }
+}
+
+void build_max_heap(int heap[], int heap_size)
+{
+  for(int i=heap_size/2; i>0;i--)
+    max_heapify(heap, i, heap_size);
 }
 
 int main()
 {
-   int heap[10] = {16 ,14, 10, 8, 7, 9, 3, 2, 4, 1};
-   max_heapify(heap, 2, 10);
+   int heap[10] = {1 ,2, 3, 4, 5, 6, 7, 8, 9, 10};
+   build_max_heap(heap, 10);
    for(int i=0; i< 10; i++)
    {
        printf("%d ", heap[i]);
    }
 }
-
-
-
